5e0102ff2d12f1a4b1841b78a18f308725fadb90
kawakami-o3 <mikamihayato383@gmail.com>
2012-10-05 13:26:28 +0900

    append

--- List/list.hs
+++ List/list.hs
@@ -1,5 +1,17 @@
-mylength :: [a] -> Int
-mylength [] = 0
-mylength (x:xs) = 1 + length xs
+import Control.Exception
+import Data.Char (isSpace)
+import Prelude hiding ((++), (!!), all, and, any, break, concat, concatMap,
+                       drop, dropWhile, elem, filter, foldl, foldr, init,
+                       last, length, lines, map, minimum, maximum, notElem,
+                       or, product, reverse, scanl, scanr, span, splitAt,
+                       sum, take, takeWhile, unlines, until, unwords, unzip,
+                       unzip3, words, zip, zip3, zipWith, zipWith3)
+import qualified Prelude ((++), concat, concatMap, map, take)
+
+
+
+length :: [a] -> Int
+length [] = 0
+length (x:xs) = 1 + length xs
 
 
--- List/list.ml
+++ List/list.ml
@@ -16,3 +16,10 @@ let rec nth lst n =
 
 let nth_test1 = nth [2; 1] 0 = 2
 let nth_test2 = nth [2; 1] 1 = 1
+
+
+let rec append a b = match a with
+  [] -> b
+  | first :: rest -> first :: (append rest b)
+
+let append_test1 = append [0] [1] = [0; 1]
