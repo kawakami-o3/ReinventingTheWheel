98c67d44b4cc594e9efa522b64b38e44e5a0fc89
kawakami-o3 <mikamihayato383@gmail.com>
2012-10-16 11:50:40 +0900

    テストコードを追加

--- List/list.hs
+++ List/list.hs
@@ -7,11 +7,97 @@ import Prelude hiding ((++), (!!), all, and, any, break, concat, concatMap,
                        sum, take, takeWhile, unlines, until, unwords, unzip,
                        unzip3, words, zip, zip3, zipWith, zipWith3)
 import qualified Prelude ((++), concat, concatMap, map, take)
+import Test.HUnit
 
+runTests :: [Test] -> IO Counts
+runTests ts = runTestTT $ TestList ts
+
+tests :: [[Test]]
+tests = [
+    appendTests
+  --, concatTests
+  --, mapTests
+  --, concatMapTests
+  --, filterTests
+  --, untilTests
+  --, andTests
+  --, orTests
+  --, anyTests
+  --, allTests
+  --, elemTests
+  --, notElemTests
+  --, zipTests
+  --, zip3Tests
+  --, zipWithTests
+  --, zipWith3Tests
+  --, unlinesTests
+  --, unwordsTests
+  --, lastTests
+  --, initTests
+  --, takeTests
+  --, dropTests
+  --, takeWhileTests
+  --, dropWhileTests
+  --, indexTests
+  --, sumTests
+  --, productTests
+  --, maximumTests
+  --, minimumTests
+  , lengthTests
+  --, reverseTests
+  --, linesTests
+  --, wordsTests
+  --, splitAtTests
+  --, spanTests
+  --, breakTests
+  --, unzipTests
+  --, unzip3Tests
+  --, foldlTests
+  --, foldrTests
+  --, scanlTests
+  --, scanrTests
+  ]
+
+
+main :: IO Counts
+main = runTests $ Prelude.concat tests
+
+assertError :: String -> a -> Assertion
+assertError msg x = handleJust errorCalls (const $ return ()) $ do
+    evaluate x         
+    assertFailure $ msg Prelude.++ ": error not thrown"
+  where
+    errorCalls (ErrorCall _) = Just ()
+
+large :: Int
+large = 2 ^ 24
+--------------------------------------------------------------------------------
+
+(++) :: [a] -> [a] -> [a]
+[] ++ y = y
+(x:xs) ++ y = x : (xs ++ y)
+
+appendTests :: [Test]
+appendTests = Prelude.map TestCase
+  [ assertEqual "[1,2] ++ [3,4]" [1,2,3,4] ([1,2] ++ [3,4])
+  , assertEqual "[] ++ [1,2]" [1,2] ([] ++ [1,2])
+  , assertEqual "[1,2] ++ []" [1,2] ([1,2] ++ [])
+  , assertEqual "[] ++ []" ([] :: [Int]) ([] ++ [])
+  ]
 
 
 length :: [a] -> Int
-length [] = 0
-length (x:xs) = 1 + length xs
+--length [] = 0
+--length (x:xs) = 1 + length xs
+length = length' 0
+  where
+    length' :: Int -> [a] -> Int
+    length' n [] = n
+    length' n (x:xs) = let m = n+1 in m `seq` length' m xs
 
+lengthTests :: [Test]
+lengthTests = Prelude.map TestCase
+  [ assertEqual "length [1,2,3]" 3 (length [1,2,3])
+  , assertEqual "length []" 0 (length [])
+  ]
 
--- List/list.ml
+++ List/list.ml
@@ -1,11 +1,18 @@
+
+(* assertTrue : string -> bool list -> unit *)
+let rec assertTrue str lst = match lst with
+  [] -> print_string (str ^ " OK.\n")
+  | first :: rest -> if first
+                      then assertTrue str rest
+                      else print_string (str ^ " NG!!!\n")
+
 (* length : 'a list -> int *)
 let rec length lst = match lst with
   [] -> 0
   | fisrt :: rest -> 1 + (length rest)
 
-let length_test1 = length [] = 0
-let length_test2 = length [1] = 1
-let length_test3 = length [3; 4; 5] = 3
+
+let length_test () = assertTrue "length" [length [] = 0; length [1] = 1; length [3; 4; 5] = 3]
 
 
 let rec nth lst n =
@@ -14,8 +21,7 @@ let rec nth lst n =
     [] -> failwith "nth"
     | first :: rest -> if n=0 then first else nth rest (n-1)
 
-let nth_test1 = nth [2; 1] 0 = 2
-let nth_test2 = nth [2; 1] 1 = 1
+let nth_test () = assertTrue "nth" [nth [2; 1] 0 = 2; nth [2; 1] 1 = 1]
 
 
 let rec append a b = match a with
@@ -25,3 +31,7 @@ let rec append a b = match a with
 let append_test1 = append [0] [1] = [0; 1]
 
 
+(* main : unit -> unit *)
+let main () = (length_test ();nth_test ())
+
+let _ = main ()
